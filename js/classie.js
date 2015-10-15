f1567a03a123c5f4284e5cd4dcac6d3494ee8279
teffet <aileluci@gmail.com>
2015-10-15 11:45:45 +0900

    map

--- js/classie.js
+++ js/classie.js
@@ -183,6 +183,15 @@ var styleArray = [
     {}
 ]
 initMap();
-
+var center;
+function calculateCenter() {
+  center = map.getCenter();
+}
+google.maps.event.addDomListener(map, 'idle', function() {
+  calculateCenter();
+});
+google.maps.event.addDomListener(window, 'resize', function() {
+  map.setCenter(center);
+});
 
 })( window );
